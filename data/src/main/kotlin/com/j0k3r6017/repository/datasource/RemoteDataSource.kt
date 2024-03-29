c3dabb51f2149bd1fad324416b15a69ad2906c7f
Daniel Chaveau <danielchaveau@gmail.com>
2024-03-29 01:01:25 -0500

    feat: Add database connection

--- data/src/main/kotlin/com/j0k3r6017/repository/datasource/RemoteDataSource.kt
+++ data/src/main/kotlin/com/j0k3r6017/repository/datasource/RemoteDataSource.kt
@@ -1,10 +1,20 @@
 package com.j0k3r6017.repository.datasource
 
 import com.j0k3r6017.model.Config
+import java.sql.Connection
+import java.sql.DriverManager
 
 class RemoteDataSource(private val config: Config) {
 
-    fun getConnection(): Any {
+    private var connection: Connection? = null
 
+    fun getConnection(): Connection {
+        Class.forName(config.driver)
+        connection = DriverManager.getConnection(
+            config.url,
+            config.username,
+            config.password
+        )
+        return connection ?: throw Exception("Connection is null")
     }
 }
